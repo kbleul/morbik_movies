c3574c845d78d51ea08a742e2480b6a92e7a0ff9
kiBROm <danu@localhost.localdomain>
2021-12-29 14:42:02 -0500

    fotter added

---
+++ src/fotter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <fotter>
+            <p>api.themoviedb.org</p>
+        </fotter>
+    )
+}
+
+export default Footer
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import Footer from './fotter'
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 );

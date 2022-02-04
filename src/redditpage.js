9c72fe85fe95dd9f3ee4282fd2d853e4966cf495
kiBROm <danu@localhost.localdomain>
2022-02-04 12:12:09 -0500

    reddit continue build 3

--- src/redditpage.js
+++ src/redditpage.js
@@ -67,18 +67,14 @@ const RedditPage = () => {
 };
 
 const SubredditPage = ({ subreddit_list, showsubreddit }) => {
-    const [subredditcontent, set_subredditcontent] = useState({})
-    const [showcontent, set_showcontent] = useState(false);
-    const [commentreplies, set_commentreplies] = useState([]);
+
     const [comment_isfetch, set_comment_isfetched] = useState(false)
     const [subredditcontent_map, set_subredditcontent_map] = useState({})
-    let commentmap = new Map();
 
     useEffect(() => {
         let counter = 0;
         let tempobj = {};
-
-        console.log("main length " + subreddit_list.length);
+        let tempobj_two = {};
 
         subreddit_list.forEach(item => {
             const fetchcomments = async () => {
@@ -91,20 +87,15 @@ const SubredditPage = ({ subreddit_list, showsubreddit }) => {
                     if (!result) { console.log("failed") }
                     tempobj[item.data.id] = result.data[0].data.children[0].data;
 
-                    commentmap.set(item.id, result.data[1].data.children)
-
                     counter++;
-                    console.log(counter)
 
                     if (counter === subreddit_list.length - 1) {
                         set_subredditcontent_map(tempobj)
-                        console.log(tempobj)
-                        console.log(subredditcontent_map)
                         set_comment_isfetched(true);
 
                     }
                 } catch (error) {
-                    console.log("Error: Fetch movies failed.");
+                    console.log("Error: Fetch reddit posts failed.");
                     tempobj[item.data.id] = {}
                 }
             }
@@ -113,85 +104,49 @@ const SubredditPage = ({ subreddit_list, showsubreddit }) => {
 
     }, [])
 
-    /*
-   const getPost = (subreddit,id, comment_title ) => {
-
-       if(comment_title[comment_title.length-1] === "?")
-       {  comment_title = comment_title.slice(0,-1); console.log(comment_title)}
    
-
-       const fetchcomments = async () => {
-           const result = await axios(` https://www.reddit.com/r/${subreddit}/comments/${id}/${comment_title}/.json`)
-      
-               set_subredditcontent(result.data[0].data.children[0].data)
-               set_commentreplies(result.data[1].data.children)
-           
-       set_showcontent(true);
-
-       }
-
-       fetchcomments();
-      
-   }
-*/
-    /*
-        const SubredditContent = () => {
-            return (<article className="subreddit_subcontainer" key={subredditcontent.id}>
-                <button onClick={() => set_showcontent(false)} className="backtofront_btn" >←</button>
-                <h2 key={subredditcontent.subreddit}>{subredditcontent.subreddit}</h2>
-    
-                <h3 key={subredditcontent.title}>{subredditcontent.title}</h3>
-                <p key={subredditcontent.selftext}>{subredditcontent.selftext}</p>
-                <p key={subredditcontent.ups}>{subredditcontent.ups}</p>
-                <p key={subredditcontent.num_comments}>{subredditcontent.num_comments}</p>
-                <p key={subredditcontent.author}>{subredditcontent.author}</p><hr />
-    
-                {commentreplies.map(item => (
-                    <section key={item.data.id}>
-                        <p key={`${item.data.id} ${item.data.author}`}>{item.data.author}</p>
-                        <p key={item.data.link_id}>{item.data.body}</p>
-                        <p key={item.data.link_id + "x"}>{item.data.replies === "" ? '' : `Replies ${item.data.replies.data.children.length} `}</p>
-                    </section>
-                ))}
-            </article>)
-        }
-    
-    
-                  { comment_isfetch && <section>
-                                <p key={subredditcontent_map[item.data.id].selftext}>{subredditcontent_map.get(item.data.id).selftext}</p>
-                                <p key={subredditcontent_map[item.data.id].ups}>{subredditcontent_map.get(item.data.id).ups}</p>
-                                <p key={subredditcontent_map.get(item.data.id).num_comments}>{subredditcontent_map.get(item.data.id).num_comments}</p>
-                                <p key={subredditcontent_map.get(item.data.id).author}>{subredditcontent_map.get(item.data.id).author}</p>
-                                <hr />
-                            </section> }
-    */
     return (<article className="reddit_subcontainer">
 
         <button onClick={() => showsubreddit(false)} className="backtofront_btn" >←</button>
+
 
         <h2 key={subreddit_list[0].data.subreddit}>{subreddit_list[0].data.subreddit}</h2>
         {
             subreddit_list.map(item => (
                 <section className="subreddit_section" key={item.data.id}>
+                 <a href={`https://www.reddit.com/${item.data.permalink}`} target="_blank">
 
+             <section className="subredditpost">     
+
+                 <div className="upvotecontainer"  key={`${subreddit_list[0].data.permalink} ${subreddit_list[0].data.permalink} ${subreddit_list[0].data.permalink} ${subreddit_list[0].data.permalink}`}>
+
+        <p  key={`${item.data.ups} ${subreddit_list[0].data.permalink}`} className="upvotepara">{item.data.ups}</p>
+
+        <p className="upvotepara_arrow" key={`${subreddit_list[0].data.permalink} ${subreddit_list[0].data.ups} ${subreddit_list[0].data.permalink}`}>
+        <svg xmlns="http://www.w3.org/2000/svg"  width="3em" height="5rem" preserveAspectRatio="xMidYMid meet" viewBox="0 0 16 16"><g fill="currentColor"><path d="M16 8A8 8 0 1 0 0 8a8 8 0 0 0 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/></g></svg></p> 
+                </div>
+
+                <div className="subredditpost_left" key={`${item.data.title} ${item.data.author}`}>
                     <h4 key={item.data.title}>{item.data.title}</h4>
-                    <p key={item.data.author}>Submitted by : {item.data.author}</p>
-                    <p key={item.data.author_fullname}>upvotes : {item.data.ups} <br /> COMMENTS</p>
 
                     { comment_isfetch && <section>{
-                        subredditcontent_map[item.data.id] !== undefined && <section key={subredditcontent_map[item.data.id].subreddit_id}>
+                        subredditcontent_map[item.data.id] !== undefined && 
+                        
+                    <section key={subredditcontent_map[item.data.id].subreddit_id}>
 
-               
                          <p key={subredditcontent_map[item.data.id].selftext}>{subredditcontent_map[item.data.id].selftext}</p>
                
-                         <p key={subredditcontent_map[item.data.id].ups}>{subredditcontent_map[item.data.id].ups}</p> 
-  
-                            <p key={subredditcontent_map[item.data.id].num_comments}>{subredditcontent_map[item.data.id].num_comments}</p>
-                           <p key={subredditcontent_map[item.data.id].author}>{subredditcontent_map[item.data.id].author}</p>
-                        <hr />
 
-                    </section>} </section>}
-
+                         <div className="subredditpost_bottom" key={`${subredditcontent_map[item.data.id].subreddit_id}${subredditcontent_map[item.data.id].subreddit_id}${item.data.author}`}>
+                    <p key={item.data.author}>Submitted by : {item.data.author}</p>
+                            <button  key={`${subredditcontent_map[item.data.id].num_comments} ${subredditcontent_map[item.data.id].subreddit_id}`} >{subredditcontent_map[item.data.id].num_comments} Comments </button>
+                            </div>
+                    </section>}
+                    
+                    </section>}
+                </div>
+                    </section> 
+                  </a>
                 </section>
             ))
         }
